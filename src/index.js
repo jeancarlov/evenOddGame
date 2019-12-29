601c6a34c6592c1d3d1297bd82fa725af313c90c
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-12-29 17:09:18 -0500

    redux store and reducer added to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import App from './components/App';
+import './index.css';
+const rootReducer = () => {
+    return { foo: 'bar'};
+};
+const store = createStore(rootReducer);
+console.log( 'store.getState()', store.getState);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log('store', store); // the first store is the string is use to identify the store object on the right
+
+ReactDOM.render(<App />, document.getElementById('root'));
