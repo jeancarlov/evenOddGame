59fa6353fe7134b0decfb4fae9d279c54c3450ab
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-12-29 18:11:23 -0500

    reducer interaction wiht the action

--- src/index.js
+++ src/index.js
@@ -3,16 +3,38 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import App from './components/App';
 import './index.css';
+import { type } from 'os';
 
 const DEFAULT_SETTINGS = {
     gameStarted: false,
     instructionsExpanded: false
 }
-const rootReducer = () => {
+
+
+const rootReducer = (state, action) => {
+    console.log('state', state, 'action', action);
+    
+    if (action.type === 'SET_GAME_STARTED'){
+        return {
+            gameStarted: action.gameStarted,
+            instructionsExpanded:false
+        }
+    }
     return DEFAULT_SETTINGS;
 };
 const store = createStore(rootReducer);
-console.log( 'store.getState()', store.getState());
+
 console.log('store', store); // the first store is the string is use to identify the store object on the right
+console.log( 'store.getState()', store.getState());
+
+store.subscribe(() => console.log('store.getstarted()', store.getState()));
+
+const action1 = { gameStarted: true, type: 'SET_GAME_STARTED' };
+store.dispatch(action1);
+
+store.dispatch({ type: 'foo'});
+store.dispatch({ type: 'bar'});
+
+// dummy action object to check  the reducer respond to the actions
 
 ReactDOM.render(<App />, document.getElementById('root'));
