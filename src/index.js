84d988e008d897b38c1cc96a88ef211d39f49d97
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-12-29 17:34:44 -0500

    reducer basic setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import App from './components/App';
 import './index.css';
+
+const DEFAULT_SETTINGS = {
+    gameStarted: false,
+    instructionsExpanded: false
+}
 const rootReducer = () => {
-    return { foo: 'bar'};
+    return DEFAULT_SETTINGS;
 };
 const store = createStore(rootReducer);
-console.log( 'store.getState()', store.getState);
-
+console.log( 'store.getState()', store.getState());
 console.log('store', store); // the first store is the string is use to identify the store object on the right
 
 ReactDOM.render(<App />, document.getElementById('root'));
