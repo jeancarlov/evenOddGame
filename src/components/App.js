d573aaab7de3d92a72971e38326500ed63198a5c
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-12-30 18:47:49 -0500

    helper functions added to prevent functions runnning directly from JSX and the inserted inside render to create UI interaction with data

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { startGame, cancelGame } from "../actions/settings"
 
 class App extends Component {
+  //local helper added to prevent the the function to be call directly in the JSX because its an anti-pattern
+  startGame = () => {
+    this.props.dispatch(startGame())
+  }
+  cancelGame = () => {
+    this.props.dispatch(cancelGame())
+  }
   render() {
     console.log('this', this);
     
+    // render different UI feature with a ternary expression
     return (
-      <div>React App</div> 
+      <div>
+        <h2> Evens or Odds</h2>
+        {
+          this.props.gameStarted ? (
+            <div>
+              <h3>The game is on ! </h3>
+              <br />
+              <button onClick = {this.cancelGame}>Cancel game</button>
+            </div>
+          ) : (
+            <div>
+              <h3> A new game awaits</h3>
+              <br />
+              <button onClick = {this.startGame}>Start game</button>
+            </div>
+          )
+        }
+      </div> 
     );
   }
 }
